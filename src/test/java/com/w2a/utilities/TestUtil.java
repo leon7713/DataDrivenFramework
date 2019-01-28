27715bc75b12b25335421478495d217653cd08d7
leon7713 <leonushar@gmail.com>
2019-01-28 20:14:36 +0200

    adding timestamp to screenshot

--- src/test/java/com/w2a/utilities/TestUtil.java
+++ src/test/java/com/w2a/utilities/TestUtil.java
@@ -4,18 +4,19 @@ import com.w2a.base.TestBase;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.testng.ITestResult;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
+import java.util.Date;
 
 public class TestUtil extends TestBase {
 
     public static void captureScreenshot(String result) throws IOException, InterruptedException {
 
+        Date d = new Date();
         File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(src, new File("C:\\test\\" + result + "screenshot.png"));
+        String myDate = d.toString().replace(":","_").replace(" ","_") + ".jpg";
+        FileUtils.copyFile(src, new File("C:\\test\\" + result + myDate));
 
     }
 }
